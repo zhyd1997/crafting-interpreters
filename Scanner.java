a4da1646c422406ce6bb81097d1e9debbc411800
张亚栋 <zhyd007@gmail.com>
2021-12-13 01:58:22 +0000

    longer lexemes.

--- Scanner.java
+++ Scanner.java
@@ -75,10 +75,120 @@ class Scanner {
         addToken(STAR);
         break;
     
+      case "!":
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+
+      case "=":
+        addToken(match("=") ? EQUAL_EQUAL: EQUAL);
+        break;
+
+      case "<":
+        addToken(match("=") ? GREATER_EQUAL : GREATER);
+        break;
+
+      case ">":
+        addToken(match("=") ? LESS_EQUAL : LESS);
+        break;
+
+      case "/":
+        if (match("/")) {
+          while (peek() != "\n" && !isAtEnd()) {
+            advance();
+          }
+        } else {
+          addToken(SLASH);
+        }
+        break;
+
+      case " ":
+      case "\r":
+      case "\t":
+        break;
+      
+      case "\n":
+        line++;
+        break;
+
+      case '"':
+        string();
+        break;
+
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek())) {
+      advance();
+    }
+
+    if (peek() != "." && isDigit(peekNext())) {
+      advance();
+
+      while (isDigit(peek())) {
+        advance();
+      }
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == "\n") {
+        line++;
+      }
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    advance();
+
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) {
+      return false;
+    }
+    if (source.charAt(current) != expected) {
+      return false;
+    }
+
+    current++;
+    return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) {
+      return "\0";
+    }
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) {
+      return "\0";
+    }
+
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    int num = Integer.parseInt(c);
+    return num >= 0 && num <= 9;
   }
 
   private char advance() {
