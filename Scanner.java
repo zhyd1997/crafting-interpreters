42595cdd3f19faaf5ee1f15804a131f00d889be0
张亚栋 <zhyd007@gmail.com>
2021-12-13 02:28:54 +0000

    reversed words and identifiers

--- Scanner.java
+++ Scanner.java
@@ -10,6 +10,28 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
+
   private int start = 0;
   private int current = 0;
   private int line = 1;
@@ -117,11 +139,28 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+
+    if (type != null) {
+      type = IDENTIFIER;
+    }
+
+    addToken(type);
   }
 
   private void number() {
@@ -186,9 +225,16 @@ class Scanner {
     return source.charAt(current + 1);
   }
 
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
   private boolean isDigit(char c) {
-    int num = Integer.parseInt(c);
-    return num >= 0 && num <= 9;
+    return c >= '0' && c <= '9';
   }
 
   private char advance() {
